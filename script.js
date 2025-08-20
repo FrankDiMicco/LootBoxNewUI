cf4e44d95706aa19b855dc10fe4d2ac40a925fca
FrankDiMicco <99362683+FrankDiMicco@users.noreply.github.com>
2025-08-20 13:36:25 -0400

    share2
    didn't do much

--- script.js
+++ script.js
@@ -688,12 +688,21 @@ class LootboxApp {
         
         if (navigator.share) {
             navigator.share({
-                title: `${lootbox.name} - Lootbox`,
+                title: `${lootbox.name} - Lootbox Creator`,
+                text: `Check out this awesome "${lootbox.name}" lootbox I created! Try your luck and see what you get! 🎲`,
                 url: url
             });
         } else {
-            navigator.clipboard.writeText(url).then(() => {
-                alert('Share link copied to clipboard!');
+            // Create nice formatted text for copying
+            const shareText = `Check out my "${lootbox.name}" lootbox! 🎁\n\nTry your luck: ${url}`;
+            
+            navigator.clipboard.writeText(shareText).then(() => {
+                alert('✨ Share message copied to clipboard!\n\nPaste it in your message to share with friends!');
+            }).catch(() => {
+                // Fallback - just copy the URL
+                navigator.clipboard.writeText(url).then(() => {
+                    alert('Share link copied to clipboard!');
+                });
             });
         }
     }
